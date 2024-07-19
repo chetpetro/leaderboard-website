5fbc2ad17d50ad31a1dc53de38e929de81a137ac
Chet Petro <chet.mh.petro@gmail.com>
2024-07-19 16:01:35 -0400

    update

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ["https://leaderboard-website-frontend.vercel.app/"],
+    origin: ["https://leaderboard-website-frontend.vercel.app"],
     credentials: true
 }))
 app.use(express.json());
