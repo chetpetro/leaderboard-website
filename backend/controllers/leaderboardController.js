136b156d77f03c36076cdff315f6c2ed5833c218
Chet <chet.mh.petro@gmail.com>
2024-07-24 15:02:33 -0400

    test3

--- backend/controllers/leaderboardController.js
+++ backend/controllers/leaderboardController.js
@@ -66,22 +66,21 @@ const createEntry = async (req, res) => {
 
     const check = await Leaderboard.findOne({ steamID });
     if (!check) return res.status(404).json({error: "No leadearboard found"});
-    const json = await check.json()
 
-    let entries = json.entries
+    let entries = check.entries
 
     for(let i = 0; i < entries.length; i++){
         if (entries[i].discordID == req.body.discordID && entries[i].time > req.body.time){
             entries[i] = req.body;
             const update = await Leaderboard.findOneAndUpdate({ steamID }, { entries });
-            res.status(200).json(update);
+            res.status(200).json({msg: 'update'});
             break;
         }
     }
 
     const response = await Leaderboard.updateOne({ steamID }, { $push : { entries: req.body }});
 
-    res.status(200).json(response);
+    res.status(200).json(response)
 }
 
 const getMOTW = async (req, res) => {
