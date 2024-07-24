5a057bdc83988d2a7c12a37dc555cc89de5e2aea
Chet <chet.mh.petro@gmail.com>
2024-07-23 22:04:07 -0400

    cron import

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const leaderboardRoutes = require('../routes/leaderboard');
 const userRoutes = require('../routes/user');
+const cronRoutes = require('../routes/cron')
 const cors = require('cors');
 
 const app = express();
