f615ff318679b7d5eed61fccf9c7b227116c44a9
Chet <chet.mh.petro@gmail.com>
2024-07-23 22:19:37 -0400

    users test

--- backend/routes/cron.js
+++ backend/routes/cron.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const newFeaturedLeaderboard = require('../controllers/cronController');
+const { loginUser, signupUser, getUser, getUsers, signupUserDiscord, loginUserDiscord } = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get("/newFeaturedLeaderboard", (req, res) => res.status(200).json({msg: "here"}));
+// Login
+router.post('/login', loginUser);
+
+// Login with discord
+router.post('/login-discord', loginUserDiscord);
+
+// Sign in 
+router.post('/sign-up', signupUser);
+
+// Sign in with Discord 
+router.post('/sign-up-discord', signupUserDiscord);
+
+// Get all users
+router.get('/', getUsers)
+
+// Get user info
+router.get('/:id', getUser);
+
 
 module.exports = router;
