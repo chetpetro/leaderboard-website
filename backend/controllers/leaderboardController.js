cc9cae5206ecf3344c09786934f2b9b9260f5827
Chet Petro <chet.mh.petro@gmail.com>
2024-08-26 11:21:26 -0400

    null fix

--- backend/controllers/leaderboardController.js
+++ backend/controllers/leaderboardController.js
@@ -84,7 +84,7 @@ const createEntry = async (req, res) => {
     let entries = map.entries
 
     for(let i = 0; i < entries.length; i++){
-        if (entries[i].discordID == req.body.discordID){
+        if (req.body.time && entries[i].discordID == req.body.discordID){
             if (entries[i].time >= req.body.time) {
                 entries[i] = req.body;
                 const update = await Leaderboard.findOneAndUpdate({ steamID }, { entries });
