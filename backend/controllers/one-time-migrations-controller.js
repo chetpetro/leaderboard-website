72eb093514b4c5ac92f3b55f8411ad837b5639c9
junker <junker@jonas-heitz.eu>
2026-04-17 10:44:12 +0200

    getting the admin users for debugging

--- backend/controllers/one-time-migrations-controller.js
+++ backend/controllers/one-time-migrations-controller.js
@@ -2,26 +2,24 @@ const User = require('../models/userModel');
 
 async function oneTimeMigrate (req, res) {
     try {
-        await setAdminUsers(req, res);
+        await getAdminUsers(req, res);
     } catch (error) {
         res.status(500).json({error: error.message});
     }
 }
 
-async function setAdminUsers(req, res) {
+async function getAdminUsers(req, res) {
     const adminDiscordIDs = [
         '261147203307831296', // Junker
         '333677894838648853'
     ]
-    const resetRes = await User.updateMany({}, {$set: {isAdmin: false}});
-
-    const adminRes = await User.updateMany(
-        {discordID: {$in: adminDiscordIDs}},
-        {$set: {isAdmin: true}});
+    const admins = User.find(
+        { discordID: {$in: adminDiscordIDs}, isAdmin: true},
+        {_id: 0, userName: 1, discordID: 1}
+    )
 
     return res.status(200).json({
-        adminRes: adminRes,
-        resetRes: resetRes,
+        admins: admins,
     })
 }
 
