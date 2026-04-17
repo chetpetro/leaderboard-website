f935a81d1d55e1186ab930926a888ba2c902906a
junker <junker@jonas-heitz.eu>
2026-04-17 10:54:48 +0200

    added "lean()" so it works

--- backend/controllers/one-time-migrations-controller.js
+++ backend/controllers/one-time-migrations-controller.js
@@ -1,26 +1,24 @@
 const User = require('../models/userModel');
 
-async function oneTimeMigrate (req, res) {
+async function oneTimeMigrate(req, res) {
     try {
-        await getAdminUsers(req, res);
+        const admins = await getAdminUsers();
+        return res.status(200).json({ admins });
     } catch (error) {
-        res.status(500).json({error: error.message});
+        return res.status(500).json({ error: error.message });
     }
 }
 
-async function getAdminUsers(req, res) {
+async function getAdminUsers() {
     const adminDiscordIDs = [
         '261147203307831296', // Junker
         '333677894838648853'
-    ]
-    const admins = User.find(
-        { discordID: {$in: adminDiscordIDs}, isAdmin: true},
-        {_id: 0, userName: 1, discordID: 1}
-    )
+    ];
 
-    return res.status(200).json({
-        admins: admins,
-    })
+    return User.find(
+        {discordID: {$in: adminDiscordIDs}, isAdmin: true},
+        {_id: 0, userName: 1, discordID: 1}
+    ).lean();
 }
 
-module.exports = {oneTimeMigrate}
+module.exports = { oneTimeMigrate };
