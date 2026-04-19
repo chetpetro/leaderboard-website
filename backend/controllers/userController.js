244bc872f9245734653d4cdc51376ec28c8b59f7
junker <junker@jonas-heitz.eu>
2026-04-19 16:13:53 +0200

    removed update function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,7 +74,6 @@ const getUser = async (req, res) => {
         return res.status(400).json({error: "User not found"})
     }
     const mapsWithUser = await Leaderboard.find({entries: {$elemMatch: {discordID: user.discordID}}})
-    updateUserPointsIfCalculationMethodChanged(user, mapsWithUser);
     const userWithEntries = await getUserWithEntries(user, mapsWithUser);
     res.status(200).json({...userWithEntries})
 }
