ed88b6b65ebd88e1f78c5abe81d31a4124fa8451
junker <mail@jonas-heitz.eu>
2026-04-27 07:40:29 +0200

    logging errors as well

--- frontend/src/context/ErrorContext.js
+++ frontend/src/context/ErrorContext.js
@@ -1,44 +1,43 @@
-import { createContext, useContext, useMemo, useState } from 'react';
+import {createContext, useContext, useMemo, useState} from 'react';
 
 const ErrorContext = createContext(null);
 
-export const ErrorProvider = ({ children }) => {
-  const [errors, setErrors] = useState([]);
+export const ErrorProvider = ({children}) => {
+    const [errors, setErrors] = useState([]);
 
+    const value = useMemo(
+        () => {
+            const showError = (message) => {
+                console.error(message);
+                const id = `${Date.now()}-${Math.floor(Math.random() * 1000)}`;
+                setErrors((prev) => [...prev, {id, message}]);
+                setTimeout(() => clearError(id), 5000);
+                return id;
+            };
 
+            const clearError = (id) => {
+                document.querySelector(`.error-message#error-id-${id}`)?.classList.add('fade-out');
+                setTimeout(() => setErrors((prev) => prev.filter((error) => error.id !== id)), 1000);
+            };
 
-  const value = useMemo(
-    () => {
-      const showError = (message) => {
-        const id = `${Date.now()}-${Math.floor(Math.random()*1000)}`;
-        setErrors((prev) => [...prev, { id, message }]);
-        setTimeout(() => clearError(id), 5000);
-        return id;
-      };
+            const clearAllErrors = () => {
+                setErrors([]);
+            };
+            return {errors, showError, clearError, clearAllErrors}
+        },
+        [errors]
+    );
 
-      const clearError = (id) => {
-        document.querySelector(`.error-message#error-id-${id}`)?.classList.add('fade-out');
-        setTimeout(() => setErrors((prev) => prev.filter((error) => error.id !== id)), 1000);
-      };
-
-      const clearAllErrors = () => {
-        setErrors([]);
-      };
-      return {errors, showError, clearError, clearAllErrors}
-    },
-    [errors]
-  );
-
-  return <ErrorContext.Provider value={value}>{children}</ErrorContext.Provider>;
+    return <ErrorContext.Provider value={value}>{children}</ErrorContext.Provider>;
 };
 
 export const useError = () => {
-  const context = useContext(ErrorContext);
+    const context = useContext(ErrorContext);
 
-  if (!context) {
-    throw new Error('useError must be used within an ErrorProvider');
-  }
+    if (!context) {
+        throw new Error('useError must be used within an ErrorProvider');
+    }
 
-  return context;
+    return context;
 };
 
