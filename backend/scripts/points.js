4bc3aa788a76945c7a23a04a4798db8a89a273b8
junker <junker@jonas-heitz.eu>
2026-04-28 00:40:08 +0200

    increased scaleFactor

--- backend/scripts/points.js
+++ backend/scripts/points.js
@@ -1,6 +1,6 @@
 const placementBonusValues = [150, 50, 25]
 const basePoints = 50;
-const scaleFactor = .1;
+const scaleFactor = .3;
 
 function currentPointCalculationMethod() {
     return `top3:${placementBonusValues[0]}:${placementBonusValues[1]}:${placementBonusValues[2]};base:${basePoints};comp:sqrt;scale:${scaleFactor}`;
