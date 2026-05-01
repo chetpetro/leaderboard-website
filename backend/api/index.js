083710583d91144610044f09e39034d85f375bac
junker <junker@jonas-heitz.eu>
2026-05-01 17:42:51 +0200

    maybe fixing it?

--- backend/api/index.js
+++ backend/api/index.js
@@ -28,13 +28,10 @@ app.use("/api/cron", cronRoutes);
 app.use("/api/oTMigrate", oTMigrationRoutes);
 app.get("/", (req, res) => res.status(200).json({ message: process.env.PORT}));
 
-const PORT = process.env.PORT;
 const URI = process.env.MONGODB_URI;
 
-mongoose.connect(URI)
-    .then(() => {
-        app.listen(PORT, () => console.log(`Connected to db & Listening on port: ${PORT}`));
-    })
-    .catch((err) => {
-        console.log('Cannot connect to db: ' + err);
-    });
+mongoose.connect(URI).catch((err) => {
+    console.log('Cannot connect to db: ' + err);
+});
+
+module.exports = app;
