6614837ddfe6ab10c2c164c75921244d7d6a8890
junker <junker@jonas-heitz.eu>
2026-05-07 23:00:31 +0200

    remove duplicate chili peppers

--- backend/controllers/one-time-migrations-controller.js
+++ backend/controllers/one-time-migrations-controller.js
@@ -1,5 +1,11 @@
 async function oneTimeMigrate(req, res) {
-    return 1+1;
+    // find leaderboards with steamID 3718005142 then remove the one with entries.length === 0
+    try {
+        await Leaderboard.deleteOne({ steamID: '3718005142', entries: { $size: 0 } });
+        res.status(200).json({ message: 'Migration completed successfully' });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = { oneTimeMigrate };
