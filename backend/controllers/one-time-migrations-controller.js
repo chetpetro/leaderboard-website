a7a20ed2cb4e4b6d60165ec098401ff6c8e3b7ef
junker <junker@jonas-heitz.eu>
2026-05-07 23:58:44 +0200

    revert otm

--- backend/controllers/one-time-migrations-controller.js
+++ backend/controllers/one-time-migrations-controller.js
@@ -1,12 +1,7 @@
 const Leaderboard = require('../models/LeaderboardModel');
 
 async function oneTimeMigrate(req, res) {
-    try {
-        await Leaderboard.deleteOne({ steamID: '3718005142', entries: { $size: 0 } });
-        res.status(200).json({ message: 'Migration completed successfully' });
-    } catch (error) {
-        console.log(error);
-    }
+    res.status(200).json({ message: 'Nothing to see here :)' });
 }
 
 module.exports = { oneTimeMigrate };
